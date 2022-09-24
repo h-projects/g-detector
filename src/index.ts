4d7dfba9770dcb3b5a78f1ce243fb9f77d783171
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2022-09-24 20:30:18 +0100

    export `allowlist` and `blocklist`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import allowlist from './allowlist.json';
+import blocklist from './blocklist.json';
+
 export * from './detector.js';
 export * from './levels.js';
+
+export { allowlist, blocklist };
