ac30c39e42dddedba8a2ea36f085d64b0288b140
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-07-28 01:59:33 +0200

    move to esm

--- tsdown.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig, type UserConfig } from 'tsdown/config';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  fixedExtension: true,
-  format: ['esm', 'cjs'],
-  sourcemap: true
-}) as UserConfig;
